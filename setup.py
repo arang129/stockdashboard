50c2a2a38a102bc95f2a56a0a7a579d171192181
arang129 <48246455+arang129@users.noreply.github.com>
2025-02-01 16:00:01 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 import setuptools
 
 setuptools.setup(
-    name="jupyter-notes-proxy",
-    version='0.0.3',
+    name="stockdashboard",
+    version='0.0.1',
     url="https://gitlab.mpcdf.mpg.de/khr/jupyter-streamlit-proxy",
     author="Klaus Reuter",
     description="klaus.reuter@mpcdf.mpg.de",
@@ -14,10 +14,10 @@ setuptools.setup(
     ],
     entry_points={
         'jupyter_serverproxy_servers': [
-            'notes = jupyter_notes_proxy:setup_notes_proxy',
+            'stockdashboard = stockdashboard:setup_stockdashboard',
         ]
     },
     package_data={
-        'jupyter_notes_proxy': ['icons/*'],
+        'stockdashboard': ['icons/*'],
     },
 )
